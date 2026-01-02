c27efaf28c07040c87a091fbed50ff377d0e11f5
anshul-303 <patilanshul27@gmail.com>
2026-01-02 11:55:53 +0530

    created a basic express server to test

--- backend/index.js
+++ backend/index.js
@@ -1,2 +1,14 @@
-console.log("Hello world! This a change file !")
-console.log('This is the second statement!')
+import express from "express"
+
+const app=express();
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+app.get("/", (req, res)=>{
+    res.status(200).json({message : "The request is received successfully!"});
+})
+
+
+app.listen(3000, ()=>{
+    console.log("The server is running on port 3000!");
+})
